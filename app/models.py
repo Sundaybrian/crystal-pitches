05726723056431c8011ce603429fb1ea9ae1abd1
Sunday Brian <brians931@gmail.com>
2019-08-04 00:31:48 +0300

    add more user model class methods for setting and veryfying the password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__='users'
@@ -8,5 +9,19 @@ class User(db.Model):
     email=db.Column(db.String(255),unique=True,index=True)
     password_hash=db.Column(db.String())
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash=generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)    
+
+
     def __repr__(self):
         return f'User {self.username}'
+
