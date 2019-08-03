00fd844eb44b387fda03e2e734925d38bd3bb4c7
Sunday Brian <brians931@gmail.com>
2019-08-03 23:20:52 +0300

    setup the manage.py with the app instance from app init module and the manager from flask-script

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,30 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
-# Initializing application
-app = Flask(__name__)
-
-
-#Initializing Flask Extensions
-bootstrap=Bootstrap(app)
+from config import config_options
 
 
 
-from app.main import views
+bootstrap=Bootstrap()
+
+
+def create_app(config_name):
+
+    # Initializing application
+    app = Flask(__name__)
+
+    #Creating the app configurations
+
+    app.config.from_object(config_options(config_name))
+
+
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    return app
+
+
+
+
 
 
 
