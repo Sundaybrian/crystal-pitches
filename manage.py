825afb14a2c92021b8905cd626a848d9390ab105
Sunday Brian <brians931@gmail.com>
2019-08-04 00:04:59 +0300

    import db instance to manage.py and the user class from app.models.Also created a shell context

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 
-
+from app.models import User
 #creating app instance
 app=create_app('development')
 
@@ -9,6 +9,10 @@ app=create_app('development')
 
 manager=Manager(app)
 manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User)
 
 if __name__=='__main__':
     manager.run()
