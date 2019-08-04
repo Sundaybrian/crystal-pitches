a7a64b40b29c8125b2033c5a781511d5bfcb255a
Sunday Brian <brians931@gmail.com>
2019-08-04 11:23:28 +0300

    add the profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,48 +1,61 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import User
 
 # Views
 @main.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
 
-    pitches=[
+  '''
+  View root page function that returns the index page and its data
+  '''
+
+  pitches=[
+      {
+          'author':"Sunday Brian0",
+          'title':'MatchdayExperience',
+          'content':'Lorem Lorem',
+          'date_posted':'July 13,2019'
+      },
         {
-            'author':"Sunday Brian0",
-            'title':'MatchdayExperience',
-            'content':'Lorem Lorem',
-            'date_posted':'July 13,2019'
-        },
-          {
-            'author':"Sunday Brian1",
-            'title':'MatchdayExperience',
-            'content':'Lorem Lorem',
-            'date_posted':'July 13,2019'
-        },
-          {
-            'author':"Sunday Brian2",
-            'title':'MatchdayExperience',
-            'content':'Lorem Lorem',
-            'date_posted':'July 13,2019'
-        },
-          {
-            'author':"Sunday Brian3",
-            'title':'MatchdayExperience',
-            'content':'Lorem Lorem',
-            'date_posted':'July 13,2019'
-        }
-    ]
-    return render_template('index.html',title='home',pitches=pitches)
+          'author':"Sunday Brian1",
+          'title':'MatchdayExperience',
+          'content':'Lorem Lorem',
+          'date_posted':'July 13,2019'
+      },
+        {
+          'author':"Sunday Brian2",
+          'title':'MatchdayExperience',
+          'content':'Lorem Lorem',
+          'date_posted':'July 13,2019'
+      },
+        {
+          'author':"Sunday Brian3",
+          'title':'MatchdayExperience',
+          'content':'Lorem Lorem',
+          'date_posted':'July 13,2019'
+      }
+  ]
+  return render_template('index.html',title='home',pitches=pitches)
 
 @main.route('/pitch/comment/new/<int:id>',methods=['GET','POST'])
 @login_required
 def new_comment(id):
-        '''
-        View function that returns a form to create a comment for a post
-        '''
+
+  '''
+  View function that returns a form to create a comment for a post
+  '''
 
 
+@main.route('/user/<uname>')
+def profile(uname):
+
+  '''
+  View function for a user profile
+  '''
+  user=User.query.filter_by(username=uname).first()
+  if user is None:
+    abort(404)
+  return render_template('profile/profile.html',user=user)  
